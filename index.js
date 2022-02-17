a335d6f774b3aeabccbfc89a976b3b54af93f7ae
Ronald Fernandez <rfernandez@softpropr.com>
2022-02-17 16:07:24 -0400

    Adds tooltip extra files

---
+++ index.js
@@ -0,0 +1,17 @@
+$( function() {
+  var tooltips = $( "[title]" ).tooltip({
+    position: {
+      my: "left top",
+      at: "right+5 top-5",
+      collision: "none"
+    }
+  });
+  $( "<button>" )
+    .text( "Show help" )
+    .button()
+    .on( "click", function() {
+      tooltips.tooltip( "open" );
+    })
+    .insertAfter( "form" );
+} );
+  
